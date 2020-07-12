022c13c007ad6020b7cbebed7b562b764b369b8a
SWAPNILXYZ <66053332+SWAPNILXYZ@users.noreply.github.com>
2020-07-12 11:19:04 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,75 +4,49 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
-var engine,world;
-var box1;
 
 function preload()
 {
-	helicopterIMG=loadImage("helicopter.png")
-	packageIMG=loadImage("package.png")
+	
 }
 
 function setup() {
 	createCanvas(800, 700);
-	rectMode(CENTER);
+
 
 	engine = Engine.create();
 	world = engine.world;
 
-	box1=new Box(500,600,15,100);
-	box2=new Box(300,600,15,100);
-	box3=new Box(400,600,200,15);
+	//Create the Bodies Here.
+	box = new Dustbin(750,200,90,10);
+    box1 = new Dustbin(790,200,10,70);
+	box2 = new Dustbin(695,200,10,70);
 
-	ground=new Ground(width/2,height-35,width,10);
-	
-	packageSprite=createSprite(width/5, 80, 10,10);
-	packageSprite.addImage(packageIMG)
-	packageSprite.scale=0.2
+	ground=new Ground(400,height-200,width,10);
 
-	helicopterSprite=createSprite(width/2, 200, 10,10);
-	helicopterSprite.addImage(helicopterIMG)
-	helicopterSprite.scale=0.6
-
-	//groundSprite=createSprite(width/2, height-35, width,10);
-	//groundSprite.shapeColor=color(255)
-
-	packageBody = Bodies.circle(width/2 , 200 , 20 , {restitution:.6, isStatic:true});
-	World.add(world, packageBody);
-	
-    
-	//Create a Ground
-	//ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
- 	//World.add(world, ground);
-
-   
+	paper = new Paper(50,200,20);
 	Engine.run(engine);
-	
+  
 }
 
 
 function draw() {
   rectMode(CENTER);
   background(0);
-  packageSprite.x= packageBody.position.x 
-  packageSprite.y= packageBody.position.y 
   
+  box.display();
   box1.display();
   box2.display();
-  box3.display();
   ground.display();
-
+  paper.display();
+  keyPressed();
   drawSprites();
  
 }
 
-
-function keyPressed() {
- if (keyCode === DOWN_ARROW) {
-    Matter.Body.setStatic(packageBody,false);
-    
-  }
+function keyPressed(){
+	if (keyCode === UP_ARROW){
+		Matter.Body.applyForce(paper.body,paper.body.position,{x:3,y:-4});
+	}
 }
-
-
 
