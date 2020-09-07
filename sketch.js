ff6f4ee43c196abe88970b67e8a5262217b05773
SWAPNILXYZ <66053332+SWAPNILXYZ@users.noreply.github.com>
2020-09-07 19:05:54 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,54 +1,63 @@
-var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
-var packageBody,ground
 const Engine = Matter.Engine;
-const World = Matter.World;
+const World= Matter.World;
 const Bodies = Matter.Bodies;
-const Body = Matter.Body;
+const Constraint = Matter.Constraint;
 
-function preload()
-{
-	
+var engine, world;
+function preload(){
+
 }
 
 function setup() {
-	createCanvas(800, 700);
+  createCanvas(800,800);
 
+  engine = Engine.create();
+    world = engine.world;
 
-	engine = Engine.create();
-	world = engine.world;
-
-	//Create the Bodies Here.
-	
-	ground = new Ground(400,500,width,10);
-
-	paper = new Paper(100,100,20,20);
-	box = new Dustbin(595,200,15,100);
-	box2 = new Dustbin(650,300,100,15);
-	box3 = new Dustbin(705,200,15,100);
-
-	Engine.run(engine);
-  
+ground = new G(width/2,  height,width, 20);
+for (var k = 0; k <= width; k = k + 80){
+  divisions.push(new Divisions(k, height - divisionHeight/2, 10, divisionHeight));
+}
+for (var j = 75; j <= width; j = j + 50){
+  plinkos.push(new P(j, 75));
+}
+for (var j = 50; j <= width - 10; j = j + 50){
+  plinkos.push(new P(j, 175));
+}
+for (var j = 75; j <= width; j = j + 50){
+  plinkos.push(new P(j, 275));
+}
+for (var j = 50; j <= width - 10; j = j + 50){
+  plinkos.push(new P(j, 375));
 }
 
+}
+
+var divisionHeight = 300;
+var plinkos = [];
+var particles = [];
+var divisions = [];
+var score = 0;
 
 function draw() {
-  rectMode(CENTER);
-  background("black");
- 
-
- 
-  paper.display();
-  box.display();
-  box2.display();
-  box3.display();
-  ground.display();
-  drawSprites();
- 
+  background(0);  
+  Engine.update(engine);
+for (var i = 0; i<plinkos.length; i++){
+  plinkos[i].display();
 }
-keyPressed();
-function keyPressed(){
-	if (keyCode === UP_ARROW){
-		Matter.Body.applyForce(paper.body,paper.body.position,{x:0.04,y:-0.07});
-	}
+  for (var j = 0; j < particles.length; j++){
+    particles[j].display();
+  }
+  for (var k = 0; k < divisions.length; k++){
+    divisions[k].display();
+  }
+
+  ground.display();
+  if (frameCount % 60 === 0){
+    particles.push(new Particles(random(width / 2 - 30, width / 2 + 30), 10, 10));
+  }
+
+  
+  drawSprites();
 }
 
