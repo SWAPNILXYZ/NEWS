c2b7201a04d2d1a3ef34e02ea00fb4318d7d1068
SWAPNILXYZ <66053332+SWAPNILXYZ@users.noreply.github.com>
2020-09-20 12:19:06 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,63 +1,56 @@
-const Engine = Matter.Engine;
-const World= Matter.World;
-const Bodies = Matter.Bodies;
-const Constraint = Matter.Constraint;
-
-var engine, world;
-function preload(){
-
+//Create variables here
+var dog, happyDog, foodS, foodStock, database;
+function preload()
+{
+  dogImage = loadImage("images/dogImg.png");
+  dogImage1 = loadImage("images/dogImg1.png");
 }
 
 function setup() {
-  createCanvas(800,800);
+  database = firebase.database();
 
-  engine = Engine.create();
-    world = engine.world;
+	createCanvas(500, 500);
+  dog = createSprite(400,400,50,60);
+  dog.addImage("dog",dogImage);
+  dog.scale = 0.20;
 
-ground = new G(width/2,  height,width, 20);
-for (var k = 0; k <= width; k = k + 80){
-  divisions.push(new Divisions(k, height - divisionHeight/2, 10, divisionHeight));
-}
-for (var j = 75; j <= width; j = j + 50){
-  plinkos.push(new P(j, 75));
-}
-for (var j = 50; j <= width - 10; j = j + 50){
-  plinkos.push(new P(j, 175));
-}
-for (var j = 75; j <= width; j = j + 50){
-  plinkos.push(new P(j, 275));
-}
-for (var j = 50; j <= width - 10; j = j + 50){
-  plinkos.push(new P(j, 375));
+  foodStock = database.ref("food");
+  foodStock.on("value", readStock);
 }
 
-}
 
-var divisionHeight = 300;
-var plinkos = [];
-var particles = [];
-var divisions = [];
-var score = 0;
+function draw() {  
+  background(46, 139, 87);
 
-function draw() {
-  background(0);  
-  Engine.update(engine);
-for (var i = 0; i<plinkos.length; i++){
-  plinkos[i].display();
-}
-  for (var j = 0; j < particles.length; j++){
-    particles[j].display();
-  }
-  for (var k = 0; k < divisions.length; k++){
-    divisions[k].display();
+  if(keyWentDown(UP_ARROW)){
+    writeStock(foodS);
+    dog.addImage("dog", dogImage1);
   }
 
-  ground.display();
-  if (frameCount % 60 === 0){
-    particles.push(new Particles(random(width / 2 - 30, width / 2 + 30), 10, 10));
-  }
-
-  
   drawSprites();
+ 
+
 }
+function readStock(data){
+ foodS = data.val();
+}
+function writeStock(x){
+ if (x <= 0){
+   x = 0;
+ }
+ else{
+   x = x-1;
+ }
+ database.ref('/').update({
+   food : x
+ })
+}
+
+
+
+
+
+
+
+
 
